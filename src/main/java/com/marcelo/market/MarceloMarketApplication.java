cdcb4953fda47a1de8f1caf9cf380b65177b9a4b
Marcelo Baez <marce.tecc@gmail.com>
2025-04-10 14:20:23 -0300

    primer commit

---
+++ src/main/java/com/marcelo/market/MarceloMarketApplication.java
@@ -0,0 +1,13 @@
+package com.marcelo.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarceloMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarceloMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelo/market/MarceloMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelo.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarceloMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
