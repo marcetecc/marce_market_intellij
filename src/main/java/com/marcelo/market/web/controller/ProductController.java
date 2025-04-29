1b5796aa45e938ac665d87118cd632427e71dc8a
Marcelo Baez <marce.tecc@gmail.com>
2025-04-28 22:26:25 -0300

    asd

--- src/main/java/com/marcelo/market/web/controller/ProductController.java
+++ src/main/java/com/marcelo/market/web/controller/ProductController.java
@@ -1,4 +1,41 @@
 package com.marcelo.market.web.controller;
 
+import com.marcelo.market.domain.Product;
+import com.marcelo.market.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/all")
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
+
+    @GetMapping("/id")
+    public Optional<Product> getProduct(@PathVariable("id") int productId) {
+        return productService.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        return productService.getByCategory(categoryId);
+    }
+
+    public Product save(Product product) {
+        return productService.save(product);
+    }
+
+    public boolean delete(int productId) {
+        return productService.delete(productId);
+    }
 }
